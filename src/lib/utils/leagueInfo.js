0f87527b3092d709ea8f3d66f9ca498a8842bd01
Chevallk <63215431+Chevallk@users.noreply.github.com>
2024-09-06 12:19:02 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "981976005080330240"; // your league ID
+export const leagueID = "1116467088593342464"; // your league ID
 export const leagueName = "League of GOATS"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
